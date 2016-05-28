815ba1990eb768aecde0ab9f2faaaba0f3cd1b7a
Steven Elleman <selleman@berkeley.edu>
2016-05-28 14:01:58 -0700

    new order

--- fields/data.js
+++ fields/data.js
@@ -1,9 +1,15 @@
 /* [address_title, title, date/location, type]*/
 var photo_links=[
 	["human_basmafam", "Day in SF", "26-5-2016 | SF, CA", "human"],
+	["human_basmaday", "Day in SF", "6-4-2016 | SF, CA", "human"],
+	["human_memonwedding", "Memon Wedding", "2-4-2016 | Fremont, CA", "human"],
+	["human_gabi", "Gabi", "23-3-2016 | SF, CA", "human"],
 	["nature_yosemite", "Yosemite", "19-3-2016 | CA", "nature"],
 	["nature_deathvalley", "Death Valley", "21-3-2016 | CA", "nature"],
+	["human_sharmeen", "Sharmeen's Wedding", "22-1-2016 | SF, CA", "human"],
+	["human_famnc", "The Fam", "Emerald Isle, NC", "human"],
 	
+
 
 
 	["nature_sunsets", "Berkeley Sunsets", "Fall 2015 | Marin County, CA", "nature"],
@@ -12,17 +18,6 @@ var photo_links=[
 	["nature_reyes", "Point Reyes", "22-3-2015 | CA", "nature"],
 	["nature_winter", "Rhode Island Winter", "14-1-2015 | RI", "nature"],
 	["nature_east", "Driving East", "19-5-2015 | CA -> RI", "nature"],
-	["nature_sur", "Big Sur", "22-3-2014 | Big Sure Nat. Park", "nature"],
-	["nature_berkeley", "East Bay", "20-11-2013 | Bay Area, CA", "nature"],
-	["nature_plantsCA", "Californian Plants", "8-8-2013 | CA", "nature"],
-	["nature_west", "Driving West", "15-6-2013 | RI -> CA", "nature"],
-	["nature_winter", "Rhode Island Winter", "25-1-2013 | RI", "nature"],
-	["nature_ri", "Rhode Island", "19-8-2012 | RI", "nature"],
-	["human_basmaday", "Day in SF", "SF, CA", "human"],
-	["human_memonwedding", "Memon Wedding", "Fremont, CA", "human"],
-	["human_gabi", "Gabi", "SF, CA", "human"], /* m */
-	["human_sharmeen", "Sharmeen's Wedding", "SF, CA", "human"],
-	["human_famnc", "The Fam", "Emerald Isle, NC", "human"], /* m */
 	["human_haightalex", "Alex in SF", "SF, CA", "human"],
 	["human_garage", "Garage Selfies", "UC Berkeley, CA", "human"],
 	["human_susanna", "The Crew", "SF, CA", "human"],
@@ -53,17 +48,8 @@ var photo_links=[
 	["human_sam", "Sam", "19-2-2015 | UCB, CA", "human"],
 	["human_wedding", "Shugufa & Nezam", "15-2-2015 | East Bay, CA", "human"],
 	["human_justin", "Justin & Jay", "19-1-2015 | SF, CA", "human"],
-	["human_spriha", "Spriha", "20-9-2014 | UCB, SF", "human"],
-	["human_kayla", "Kayla", "31-8-2014 | SF", "human"],
-	["human_ray", "Ray", "31-8-2104 | SF", "human"],
-	["human_dallin", "Dallin & Jordan", "30-8-2104 | UCB, CA", "human"],
-	["human_ananya", "Ananya", "2-8-2014 | UCB, CA", "human"],
-	["human_pride", "Pride", "29-6-2014 | SF, CA", "human"],
-	["human_holi", "Holi", "3-5-2014 | UCB, CA", "human"],
-	["human_freshta", "Freshta", "12-12-2013 | UCB, CA", "human"],
-	["human_mirrors", "Mirrors: Self-Portraits", "2-12-2012 | RI", "human"],
-	["human_haley", "Haley", "26-10-2012 | RI", "human"],
-	["travel_paris", "Paris", "19-8-2105 | France", "travel"],
+
+	["travel_paris", "Paris", "19-8-2015 | France", "travel"],
 	["travel_nantes", "Nantes", "18-8-2015 | France", "travel"],
 	["travel_madrid", "Madrid & Barcelona", "16-8-2015 | Spain", "travel"],
 	["travel_lisbon", "Lisbon", "14-8-2015 | Portugal", "travel"],
@@ -84,8 +70,35 @@ var photo_links=[
 	["travel_prague", "Prague", "18-7-2015 | Czech Republic", "travel"],
 	["travel_berlin", "Berlin", "16-7-2015 | Germany", "travel"],
 	["travel_la", "Los Angeles", "9-7-2015 | CA", "travel"],
+
+
+	
 	["travel_nyc2", "New York City", "1-1-2015 | NY", "travel"],
 	["travel_treasure", "Yerba Buena", "19-1-2015 | SF", "travel"],
+
+
+
+
+	["nature_sur", "Big Sur", "22-3-2014 | Big Sure Nat. Park", "nature"],
+	["nature_berkeley", "East Bay", "20-11-2013 | Bay Area, CA", "nature"],
+	["nature_plantsCA", "Californian Plants", "8-8-2013 | CA", "nature"],
+	["nature_west", "Driving West", "15-6-2013 | RI -> CA", "nature"],
+	["nature_winter", "Rhode Island Winter", "25-1-2013 | RI", "nature"],
+	["nature_ri", "Rhode Island", "19-8-2012 | RI", "nature"],
+	
+	
+	["human_spriha", "Spriha", "20-9-2014 | UCB, SF", "human"],
+	["human_kayla", "Kayla", "31-8-2014 | SF", "human"],
+	["human_ray", "Ray", "31-8-2104 | SF", "human"],
+	["human_dallin", "Dallin & Jordan", "30-8-2104 | UCB, CA", "human"],
+	["human_ananya", "Ananya", "2-8-2014 | UCB, CA", "human"],
+	["human_pride", "Pride", "29-6-2014 | SF, CA", "human"],
+	["human_holi", "Holi", "3-5-2014 | UCB, CA", "human"],
+	["human_freshta", "Freshta", "12-12-2013 | UCB, CA", "human"],
+	["human_mirrors", "Mirrors: Self-Portraits", "2-12-2012 | RI", "human"],
+	["human_haley", "Haley", "26-10-2012 | RI", "human"],
+
+	
 	["travel_nyc1", "New York City", "14-6-2014 | NY", "travel"],
 	["travel_chinatown", "Chinatown", "16-6-2014 | SF, CA", "travel"],
 	["travel_easter", "Easter Island", "28-12-2013 | Chile", "travel"],
